49595329384c5365d70c9606ef9791550e54323b
ali7d <98466570+ali7d@users.noreply.github.com>
2023-01-17 11:00:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ import requests
 from tkinter import filedialog
 from PIL import ImageTk, Image
 import io
+import urllib.request
 
 # Initialize the GUI
 window = tk.Tk()
-window.title("Text to Image Generator")
-window.geometry("800x600")
+window.title("Lite Stable Generator")
+window.geometry("932x1232")
+window.config(bg="#979797")
 
 # Add a label and text box for the user to enter a text description
-label = tk.Label(master=window, text="Enter a text description:", font=("Arial", 14))
+label = tk.Label(master=window, bg="#cfcfcf", text="Enter Your Prompt:", font=("Consolas", 14))
 label.pack(pady=20)
 
-text_box = tk.Text(master=window, height=10, width=50, font=("Arial", 12))
+text_box = tk.Text(master=window, bg="#addb9c", height=10, width=50, font=("Consolas", 12))
 text_box.pack(pady=10)
 
 # Add a generate button
@@ -31,6 +33,8 @@ def generate():
   response = requests.post("https://api.newnative.ai/stable-diffusion?prompt=futuristic spce station, 4k digital art", headers=headers, json=data)
   image = response.content
 
+  urllib.request.urlretrieve("https://storage.googleapis.com/lablab-static-eu/stable-diffusion/futuristic%20space%20station.png", "local-filename.jpg")
+
   # Convert the image to a PhotoImage object
   image = ImageTk.PhotoImage(image=Image.open(io.BytesIO(image)))
 
@@ -39,7 +43,7 @@ def generate():
   image_label.image = image
   image_label.pack(pady=20)
 
-button = tk.Button(master=window, text="Generate", command=generate)
+button = tk.Button(master=window, bg="#d3d3d3",text="Generate", command=generate)
 button.pack()
 
 # Run the GUI
